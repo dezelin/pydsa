0954cfa0d03443854aea31917a5dcce11174f7b0
Aleksandar Dezelin <dezelin@gmail.com>
2014-11-15 02:05:09 +0100

    Implemented Stack + unittest.

---
+++ structs/stack.py
@@ -0,0 +1,81 @@
+
+import unittest
+
+
+class Stack(object):
+    """
+    Stack data structure implementation using built-in list.
+    """
+
+    def __init__(self):
+        """ Constructor """
+        self.items = []
+
+    def push(self, x):
+        """ Pushes element x on top of the stack """
+        self.items.append(x)
+
+    def pop(self):
+        """ Pops the element from the top """
+        return self.items.pop()
+
+    def size(self):
+        """ Returns the size of the stack """
+        return len(self.items)
+
+    def is_empty(self):
+        """ Returns True if the stack is empty """
+        return self.size() == 0
+
+
+class TestStack(unittest.TestCase):
+
+    def test__init__(self):
+        stack = Stack()
+        assert stack.items == []
+
+    def test_push(self):
+        stack = Stack()
+        assert len(stack.items) == 0
+        for i in xrange(1, 10):
+            stack.push(i)
+            assert len(stack.items) == i
+            assert stack.items[i - 1] == i
+
+        assert stack.items == [i for i in xrange(1, 10)]
+
+    def test_pop(self):
+        stack = Stack()
+        assert len(stack.items) == 0
+        for i in xrange(1, 10):
+            stack.push(i)
+            assert len(stack.items) == i
+
+        assert stack.items == [i for i in xrange(1, 10)]
+
+        for i in xrange(9, 0, -1):
+            x = stack.pop()
+            assert x == i
+
+        assert stack.items == []
+
+    def test_size(self):
+        stack = Stack()
+        assert stack.size() == 0
+        for i in xrange(1, 10):
+            stack.push(i)
+            assert stack.size() == i
+
+        assert stack.items == [i for i in xrange(1, 10)]
+
+    def test_is_empty(self):
+        stack = Stack()
+        assert stack.is_empty()
+        for i in xrange(1, 10):
+            stack.push(i)
+            assert not stack.is_empty()
+
+        assert stack.items == [i for i in xrange(1, 10)]
+
+if __name__ == "__main__":
+    unittest.main(module="stack", exit=False)
